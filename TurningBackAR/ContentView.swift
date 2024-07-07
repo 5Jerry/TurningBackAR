2ff1dbe9d59babb8b31b211cbc92012e694dea09
Jerry <red5trainjerry@gmail.com>
2024-07-07 21:36:38 +0800

    Full working functionality of turning mechanism to show AR model

--- TurningBackAR/ContentView.swift
+++ TurningBackAR/ContentView.swift
@@ -7,31 +7,100 @@
 
 import SwiftUI
 import RealityKit
+import CoreMotion
 
 struct ContentView : View {
+    
+    @State var roll: Double = 0.0
+    @State var pitch: Double = 0.0
+    @State var yaw: Double = 0.0
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack {
+            ARViewContainer(roll: $roll, pitch: $pitch, yaw: $yaw).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Device Attitude")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Roll: \(roll, specifier: "%.2f")")
+                Text("Pitch: \(pitch, specifier: "%.2f")")
+                Text("Yaw: \(yaw, specifier: "%.2f")")
+            }
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    var motionManager = CMMotionManager()
+    
+    @State var isShowingModel: Bool = false
+    
+    @Binding var roll: Double
+    @Binding var pitch: Double
+    @Binding var yaw: Double
     
     func makeUIView(context: Context) -> ARView {
-        
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+        self.startDeviceMotionUpdates()
         return arView
-        
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ arView: ARView, context: Context) {
+        var completion: ((Entity?) -> Void)? = { _ in }
+        
+        // Create new model when isShowingModel is true
+        if isShowingModel {
+            // Load the "Scene" scene from the "Experience" Reality File
+            let boxAnchor = try! Experience.loadScene()
+            
+            // Add the box anchor to the scene
+            arView.scene.anchors.append(boxAnchor)
+            
+            completion = { _ in
+                print("Both actions were completely stopped.")
+                self.startDeviceMotionUpdates()
+                isShowingModel = false
+            }
+            
+            boxAnchor.actions.done.onAction = completion
+        } else {
+            arView.scene.anchors.removeAll()
+        }
+    }
     
+    func startDeviceMotionUpdates() {
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.1
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { [self] (motionData, error) in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                    return
+                }
+                
+                if let attitude = motionData?.attitude {
+                    DispatchQueue.main.async {
+                        self.roll = attitude.roll * 180 / .pi
+                        self.pitch = attitude.pitch * 180 / .pi
+                        self.yaw = attitude.yaw * 180 / .pi
+                        
+                        if abs(attitude.yaw * 180 / .pi) > 160 {
+                            self.isShowingModel = true
+                            self.stopDeviceMotionUpdates()
+                        }
+                    }
+                }
+            }
+        } else {
+            print("Device motion is not available")
+        }
+    }
+    
+    func stopDeviceMotionUpdates() {
+        motionManager.stopDeviceMotionUpdates()
+        print("Device motion updates stopped")
+    }
 }
 
 #if DEBUG
